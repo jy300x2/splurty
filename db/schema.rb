ca6938a93e1449fd9136c28a5bb3927596d79652
Jon Yu <jy300x2@gmail.com>
2016-05-24 02:21:16 +0000

    Add mobile meta tag

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160517222539) do
+ActiveRecord::Schema.define(version: 20160522090741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160517222539) do
     t.string   "author"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "source"
   end
 
 end
